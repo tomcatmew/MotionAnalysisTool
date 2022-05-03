f45b269d1f0ecefbc988d2744953da2c8755c755
tomcatmew <helltomcat@gmail.com>
2022-05-03 17:14:57 +0900

    add old files

--- script.js
+++ script.js
@@ -4,11 +4,23 @@ const clock = new THREE.Clock();
 
 let camera, controls, scene, renderer, gui;
 let mixer, skeletonHelper, boneContainer, fname, bvhName;
+let mixer_action;
+let line;
 const loader = new THREE.BVHLoader();
+var skeletonHistory = {
+  root: [],
+  root_line: null,
+  root_total_frame: 0,
+  leftLeg: [],
+}
 
-fname = "data/LocomotionFlat01_000.bvh";
+// fname = "data/LocomotionFlat01_000.bvh";
+fname = "data/08_04.bvh";
 var settings = {
   message: fname,
+  t_scale: 1,
+  show_root_path: false,
+  pause: false,
 };
 
 var params = {
@@ -18,7 +30,6 @@ var params = {
       var file = input.files[0];
       fname = file.name;
       f_src = URL.createObjectURL(file)
-      console.log(f_src);
       // update all controllers
       loader.load(f_src, function (result) {
         bvhName.setValue(fname);
@@ -29,24 +40,75 @@ var params = {
         skeletonHelper.skeleton = result.skeleton; // allow animation mixer to bind to THREE.SkeletonHelper directly
 
         boneContainer = new THREE.Group();
+
         boneContainer.add(result.skeleton.bones[0]);
-        for (var i = 0; i < boneContainer.children.length; i++) {
-          boneContainer.scale.set(5, 5, 5);
-        }
+        console.log(result.skeleton.bones.length);
+        console.log(boneContainer.children.length);
+        // for (var i = 0; i < boneContainer.children.length; i++) {
+        boneContainer.scale.set(5, 5, 5);
+        // }
         scene.add(skeletonHelper);
         scene.add(boneContainer);
-
+        reset_skeletonHistory();
         // play animation
         mixer = new THREE.AnimationMixer(skeletonHelper);
-        mixer.clipAction(result.clip).setEffectiveWeight(1.0).play();
+        mixer_action = mixer.clipAction(result.clip);
+        mixer_action.setEffectiveWeight(1.0).play();
+        skeletonHistory.root_total_frame = mixer_action.getClip().tracks[0].times.length;
       });
     });
     input.click();
   },
 };
 
-init();
-animate();
+function reset_skeletonHistory() {
+  skeletonHistory.root = [];
+  skeletonHistory.root_line = null;
+  skeletonHistory.root_total_frame = 0;
+  skeletonHistory.leftLeg = [];
+}
+
+function compare_vector(a, b) {
+  if (a.x == b.x && a.y == b.y && a.z == b.z) return true;
+}
+
+function int(a) {
+  return Math.floor(a);
+}
+
+function debugJoint() {
+
+  var pos = new THREE.Vector3();
+  skeletonHelper.skeleton.bones[0].getWorldPosition(pos);
+  // console.log(int(skeletonHistory.root_total_frame / 2))
+  if (mixer_action.paused == false) {
+    if (skeletonHistory.root.length < int(skeletonHistory.root_total_frame / 2) - 2) {
+      skeletonHistory.root.push(pos);
+    }
+  }
+  if (settings.show_root_path) {
+    if (skeletonHistory.line) scene.remove(skeletonHistory.line);
+    const geometry = new THREE.BufferGeometry().setFromPoints(skeletonHistory.root);
+    const material = new THREE.LineBasicMaterial({ color: 0x000000 });
+    skeletonHistory.line = new THREE.Line(geometry, material);
+    scene.add(skeletonHistory.line);
+  }
+  else {
+    if (skeletonHistory.line) scene.remove(skeletonHistory.line);
+  }
+
+  // let j = []
+  // let pos = new THREE.Vector3();
+  // root = mixer.getRoot();
+  // root.getWorldPosition(pos);
+  // j.push(pos);
+  // console.log(j[0].x);
+  // console.log(j[0].y);
+  // console.log(j[0].z);
+}
+
+
+
 
 loader.load(fname, function (result) {
   skeletonHelper = new THREE.SkeletonHelper(result.skeleton.bones[0]);
@@ -60,9 +122,20 @@ loader.load(fname, function (result) {
   scene.add(skeletonHelper);
   scene.add(boneContainer);
 
+  let j = []
+  let pos = new THREE.Vector3();
+  skeletonHelper.skeleton.bones[0].getWorldPosition(pos);
+  j.push(pos);
+  console.log(j[0].x);
+  console.log(j[0].y);
+  console.log(j[0].z);
+
   // play animation
   mixer = new THREE.AnimationMixer(skeletonHelper);
-  mixer.clipAction(result.clip).setEffectiveWeight(1.0).play();
+  // mixer.clipAction(result.clip).setEffectiveWeight(1.0).play();
+  mixer_action = mixer.clipAction(result.clip);
+  mixer_action.setEffectiveWeight(1.0).play();
+  skeletonHistory.root_total_frame = mixer_action.getClip().tracks[0].times.length;
 });
 
 function init() {
@@ -84,6 +157,8 @@ function init() {
   gui.domElement.id = "gui";
   var bvhLoadUI = gui.add(params, "loadFile").name("Load BVH file 📁");
   bvhName = gui.add(settings, "message").name("File Name");
+  gui.add(settings, 'pause').name("Pause").onChange(() => { mixer_action.paused = mixer_action.paused == true ? false : true });
+  gui.add(settings, 'show_root_path').name("Show Root Bone Path");
   bvhName.domElement.style.pointerEvents = "none";
 
   // renderer
@@ -110,12 +185,16 @@ function onWindowResize() {
   renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
+
 function animate() {
   requestAnimationFrame(animate);
 
   const delta = clock.getDelta();
-
   if (mixer) mixer.update(delta);
-
+  if (skeletonHelper) debugJoint();
   renderer.render(scene, camera);
 }
+
+
+init();
+animate();
